5e0fb63bc89d67c95f4c7d7e50802c09be8eed0c
Ricardo Mello <ricardo.mello@outlook.com>
2019-03-02 16:32:18 -0300

    arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [{ name: 'Pepperoni', toppings: ['pepperoni'] }];
+
+const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: 'Blazing Inferno',
+  getName: function() {
+    return this.name;
+  }
+};
+
+console.log(pizza.getName());
