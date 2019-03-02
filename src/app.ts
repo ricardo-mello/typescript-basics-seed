bf4e50958b51b2bf984622567af69126ad667501
Ricardo Mello <ricardo.mello@outlook.com>
2019-03-02 17:06:07 -0300

    object literal improvements

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-function multiply(a: number, b = 25) {
-  return a * b;
+const pizza = {
+  name: 'Pepperoni',
+  price: 15,
+  getName() {
+    console.log(this.name);
+  }
+};
+
+console.log(pizza.getName());
+
+const toppings = ['pepperoni'];
+
+function createOrder(pizza: any, toppings: any) {
+  return { pizza, toppings };
 }
 
-console.log(multiply(5));
-console.log(multiply(5, 35));
+console.log(createOrder(pizza, toppings));
